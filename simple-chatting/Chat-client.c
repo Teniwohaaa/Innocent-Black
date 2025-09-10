d31904101cb817fb645910f8ec7bde4e09642815
Teniwoha <meliodart70@gmail.com>
2025-09-10 23:13:12 +0100

    modified:   simple-chatting/Chat-client.c 	new file:   simple-chatting/SocketUtil.h

--- simple-chatting/Chat-client.c
+++ simple-chatting/Chat-client.c
@@ -1,8 +1,7 @@
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// mingw32-make
+
+#include "SocketUtil.h"
+
 #pragma comment(lib, "ws2_32.lib")
 
 #define BUFFER_SIZE 2048
@@ -10,28 +9,19 @@
 #define error(msg, ...) printf("[-] " msg "\n", ##__VA_ARGS__)
 #define info(msg, ...) printf("[*] " msg "\n", ##__VA_ARGS__)
 
-SOCKET CreateSocketTCPIPv4(void) {
-    return socket(AF_INET, SOCK_STREAM, 0);
-}
-
-struct sockaddr_in CreateIPv4Address(const char *IP, int PORT) {
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(PORT);
-    address.sin_addr.s_addr = inet_addr(IP);
-    return address;
-}
-
-int main() {
+int main()
+{
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
         error("WSAStartup failed");
         return EXIT_FAILURE;
     }
 
     info("creating the socket");
     SOCKET socketFD = CreateSocketTCPIPv4();
-    if (socketFD == INVALID_SOCKET) {
+    if (socketFD == INVALID_SOCKET)
+    {
         error("Socket creation failed, error:%d", WSAGetLastError());
         WSACleanup();
         return EXIT_FAILURE;
@@ -44,7 +34,8 @@ int main() {
 
     info("trying to connect...");
     int iResult = connect(socketFD, (struct sockaddr *)&address, sizeof(address));
-    if (iResult == SOCKET_ERROR) {
+    if (iResult == SOCKET_ERROR)
+    {
         error("Connection failed, ip: %s port: %d error: %d", IP, PORT, WSAGetLastError());
         closesocket(socketFD);
         WSACleanup();
@@ -61,7 +52,8 @@ int main() {
 
     char response[BUFFER_SIZE];
     int bytesReceived = recv(socketFD, response, BUFFER_SIZE - 1, 0);
-    if (bytesReceived > 0) {
+    if (bytesReceived > 0)
+    {
         response[bytesReceived] = '\0';
         info("received data was: %s", response);
     }
---
+++ simple-chatting/SocketUtil.h
@@ -0,0 +1,19 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+SOCKET CreateSocketTCPIPv4(void)
+{
+    return socket(AF_INET, SOCK_STREAM, 0);
+}
+
+struct sockaddr_in CreateIPv4Address(const char *IP, int PORT)
+{
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    address.sin_addr.s_addr = inet_addr(IP);
+    return address;
+}
